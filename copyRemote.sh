d18fbf39c8efa383ddec282709e818d2c7d6f43a
gaiapoli <42361438+gaiapoli@users.noreply.github.com>
2018-08-22 01:16:35 +0200

    update the directories

--- copyRemote.sh
+++ copyRemote.sh
@@ -2,7 +2,7 @@ remoteUser="pi"
 remoteHost="192.168.1.120"
 
 
-ssh $remoteUser@$remoteHost "rm -r ShitBuster; mkdir ShitBuster"
+ssh $remoteUser@$remoteHost "mkdir ShitBuster; cd ShitBuster; mkdir shitmodules"
 
 scp ./*.* "$remoteUser@$remoteHost:/home/$remoteUser/ShitBuster"
-
+scp ./shitmodules/*.* "$remoteUser@$remoteHost:/home/$remoteUser/ShitBuster/shitmodules"
