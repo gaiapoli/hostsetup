b94d7b602187971d013ac3d2c359afb7671e9ebe
frpol <killereskimo@hotmail.com>
2018-08-22 01:02:37 +0200

    fix $ in remoteuser

--- copyRemote.sh
+++ copyRemote.sh
@@ -4,5 +4,5 @@ remoteHost="192.168.1.120"
 
 ssh $remoteUser@$remoteHost "rm -r ShitBuster; mkdir ShitBuster"
 
-scp ./*.* "$remoteUser@$remoteHost:/home/remoteUser/ShitBuster"
+scp ./*.* "$remoteUser@$remoteHost:/home/$remoteUser/ShitBuster"
 
