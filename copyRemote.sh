439aa4a5a87219eead15cbf5e270dcc04402b3b3
gaiapoli <42361438+gaiapoli@users.noreply.github.com>
2018-08-22 01:13:52 +0200

    update

--- copyRemote.sh
+++ copyRemote.sh
@@ -2,7 +2,8 @@ remoteUser="pi"
 remoteHost="192.168.1.120"
 
 
-ssh $remoteUser@$remoteHost "rm -r ShitBuster; mkdir ShitBuster"
+ssh $remoteUser@$remoteHost "mkdir ShitBuster"
+ssh $remoteUser@$remoteHost "cd ShitBuster; mkdir shitmodules"
 
 scp ./*.* "$remoteUser@$remoteHost:/home/$remoteUser/ShitBuster"
-
+scp ./*.* "$remoteUser@$remoteHost:/home/$remoteUser/ShitBuster/shitmodules"
